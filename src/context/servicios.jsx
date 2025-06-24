18446c4085aab4a3afd9949a498f2ba5c231a7d3
DiegoSanBal <diegosanturtun@gmail.com>
2025-06-24 18:19:46 -0300

    cree nueva logica para guardar las partidas

--- src/context/servicios.jsx
+++ src/context/servicios.jsx
@@ -18,6 +18,7 @@ export const getMatches = async (summonerName, tagLine) => {
       `https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/${puuid}/ids?start=0&count=10&api_key=${API_KEY}`
     );
     const matchIds = await resMatchIds.json();
+
     if (!Array.isArray(matchIds)) {
       console.error("matchIds no es un array", matchIds);
       return [];
@@ -25,14 +26,27 @@ export const getMatches = async (summonerName, tagLine) => {
 
     const matches = await Promise.all(
       matchIds.map(async (id) => {
+        // Verificamos si ya existe en MockAPI
+        const existingMatch = await fetch(
+          `https://683fa1935b39a8039a552628.mockapi.io/api/v1/matches?matchId=${id}`
+        );
+        const existing = await existingMatch.json();
+
+        if (existing.length > 0) {
+          return existing[0];
+        }
+
+        // Si no existe, se llama a la API
         const resMatch = await fetch(
           `https://americas.api.riotgames.com/lol/match/v5/matches/${id}?api_key=${API_KEY}`
         );
         const data = await resMatch.json();
+
         const player = data.info.participants.find((p) => p.puuid === puuid);
 
-        return {
+        const match = {
           matchId: id,
+          userId: userId,
           champion: player.championName,
           kills: player.kills,
           deaths: player.deaths,
@@ -41,9 +55,24 @@ export const getMatches = async (summonerName, tagLine) => {
           mode: data.info.gameMode,
           duration: Math.floor(data.info.gameDuration / 60),
           timestamp: data.info.gameStartTimestamp,
+          likes: [],
         };
+
+        // Se guarda en mockAPI
+        const resSaved = await fetch(
+          "https://683fa1935b39a8039a552628.mockapi.io/api/v1/matches",
+          {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(match),
+          }
+        );
+
+        const saved = await resSaved.json();
+        return saved;
       })
     );
+
     return matches;
   } catch (error) {
     console.error("Error en getMatches", error);
